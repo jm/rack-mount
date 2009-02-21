b184f41413e41a420b9f64244e8e097837630387
Joshua Peek <josh@joshpeek.com>
2009-02-21 12:16:09 -0600

    add basic graphviz support

--- lib/rack/mount/route_set.rb
+++ lib/rack/mount/route_set.rb
@@ -27,7 +27,7 @@ module Rack
         else
           path = route.path.slice(SegmentString::FIRST_SEGMENT_REGEXP)
           route.methods.each do |method|
-            self["#{method}#{path}"] = route
+            self["#{method} /#{path}"] = route
           end
         end
 
@@ -38,7 +38,7 @@ module Rack
         method = env["REQUEST_METHOD"]
         path = env["PATH_INFO"]
 
-        key = "#{method}#{path.slice(SegmentString::FIRST_SEGMENT_REGEXP)}"
+        key = "#{method} /#{path.slice(SegmentString::FIRST_SEGMENT_REGEXP)}"
         self[key].each do |route|
           result = route.call(env)
           return result unless result[0] == 404
@@ -48,6 +48,31 @@ module Rack
 
       def worst_case
         values.max { |a, b| a.length <=> b.length }.length
+      end
+
+      def to_graph
+        graph = <<-EOS
+digraph G {
+  nodesep=.05;
+  rankdir=LR;
+  node [shape=record,width=.1,height=.1];
+
+  node0 [label = "#{keys.map { |k| "<k#{k.object_id}> #{k}" }.join("|")}",height=2.0];
+
+  node [width = 1.5];
+  #{
+    values.map { |v|
+      "node#{v.object_id} [label = \"{<n> #{v.map { |v| " /#{v.path} " }.join("|")} }\"];"
+    }.join("\n  ")
+  }
+
+  #{
+    map { |k, v|
+      "node0:k#{k.object_id} -> node#{v.object_id}:n;"
+    }.join("\n  ")
+  }
+}
+        EOS
       end
     end
   end
