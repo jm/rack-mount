e75ca4c988184bf5022fa23641039c95aea91633
Joshua Peek <josh@joshpeek.com>
2009-02-21 11:41:13 -0600

    merge routeset and bucket

--- lib/rack/mount/route_set.rb
+++ lib/rack/mount/route_set.rb
@@ -1,10 +1,6 @@
 module Rack
   module Mount
-    class RouteSet
-      def initialize
-        @routes = []
-      end
-
+    class RouteSet < Bucket
       def draw
         yield Mappers::RailsClassic.new(self)
         freeze
@@ -25,13 +21,17 @@ module Rack
 
       def add_route(options = {})
         route = Route.new(options)
-        @routes << route
-        route
-      end
 
-      def freeze
-        @buckets = build_tree
-        super
+        if route.dynamic?
+          self << route
+        else
+          path = route.path.slice(SegmentString::FIRST_SEGMENT_REGEXP)
+          route.methods.each do |method|
+            self["#{method}#{path}"] = route
+          end
+        end
+
+        route
       end
 
       def call(env)
@@ -39,36 +39,16 @@ module Rack
         path = env["PATH_INFO"]
 
         key = "#{method}#{path.slice(SegmentString::FIRST_SEGMENT_REGEXP)}"
-        @buckets[key].each do |route|
+        self[key].each do |route|
           result = route.call(env)
           return result unless result[0] == 404
         end
         nil
       end
 
-      def size
-        @routes.length
-      end
-
       def worst_case
-        @buckets.values.max { |a, b| a.length <=> b.length }.length
+        values.max { |a, b| a.length <=> b.length }.length
       end
-
-      private
-        def build_tree
-          bucket = Bucket.new
-          @routes.each do |route|
-            if route.dynamic?
-              bucket << route
-            else
-              route.methods.each do |method|
-                key = "#{method}#{route.path.slice(SegmentString::FIRST_SEGMENT_REGEXP)}"
-                bucket[key] = route
-              end
-            end
-          end
-          bucket.freeze
-        end
     end
   end
 end
